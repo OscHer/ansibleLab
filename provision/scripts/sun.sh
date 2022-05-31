8e8da0cb33c448f8a705fdb4127970fbed6d6473
oscar_heredia <9662727-OscHer@users.noreply.gitlab.com>
2022-05-31 09:59:15 +0200

    Add ssh-keygen to Sun
    We want to automatically add ssh keys to every node;
    so we must ensure a new public key from master node
    is generated and copied to every managed node.

--- provision/scripts/sun.sh
+++ provision/scripts/sun.sh
@@ -4,3 +4,5 @@
 yum -y install epel-release # Ansible is not included in base Rocky repos
 yum -y install ansible vim git
 ansible --version # Check if ansible is installed. This will be shown in 'vagrant up' post message
+rm -f ~/.ssh/id_rsa
+ssh-keygen -q -t rsa -f ~/.ssh/id_rsa -b 2048 -N ""
