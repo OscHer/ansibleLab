ff8f05cc4e0c6661e5d712f374dc76a73159bb06
oscar_heredia <9662727-OscHer@users.noreply.gitlab.com>
2022-06-01 10:22:38 +0200

    Change ansible execution to vagrant user

--- provision/scripts/ansible-check.sh
+++ provision/scripts/ansible-check.sh
@@ -2,4 +2,4 @@
 
 MYCOLLECTION=mercury
 echo $MYCOLLECTION > provisional_inventory
-ansible $MYCOLLECTION -m ping -u vagrant  -i provisional_inventory
+su -c "ansible $MYCOLLECTION -m ping -u vagrant  -i provisional_inventory" vagrant 
